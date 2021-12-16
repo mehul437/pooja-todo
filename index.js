86401c2eb5a2984db80c8a50fc69719ae665c642
mehul 437 <mehulkaushal.sq@gmail.com>
2021-12-16 21:58:26 +0530

    done

--- index.js
+++ index.js
@@ -1,512 +1,237 @@
-const clic=()=>{
-    var bigbox=document.createElement("div");
-    bigbox.setAttribute("id","bigbox1");
-    
-    
-    var inside=document.createElement("label");
-    inside.innerHTML="ADD NEW LIST";
-    inside.setAttribute("for","insidee");
-    bigbox.appendChild(inside);
+function create() {
+    if (document.getElementById("innermain").contains(document.getElementById("empty"))) {
+        var emp = document.getElementById("empty")
+        emp.remove()
+    }
 
-    var inside1=document.createElement("input");
-    inside1.setAttribute("placeholder","Enter the list")
-    inside1.setAttribute("id","insidee1");
-    inside1.setAttribute("name","text");
-    inside1.setAttribute("type","text");
-    bigbox.appendChild(inside1);
-
-    var inside2=document.createElement("button");
-    inside2.innerHTML="ADD";
-    inside2.setAttribute("id","insidee2");
-    inside2.setAttribute("type","submit");
-    inside2.setAttribute("onclick","goo()");
-    bigbox.appendChild(inside2);
-
-    var inside3=document.createElement("button");
-    inside3.innerHTML="CLOSE";
-    inside3.setAttribute("id","insidee3");
-    inside3.setAttribute("onclick","goo1()");
-    inside3.setAttribute("type","submit");
-    bigbox.appendChild(inside3);
-
-    document.getElementById("top").appendChild(bigbox);
-
-    var q=document.getElementById("top1");
-    q.style.filter="blur(10px)";
-    
-
-}
-var cap;
-function goo(){
-    var capture=document.getElementById("insidee1");
-    cap=capture.value;
-    //console.log(cap);
-
-    var q=document.getElementById("top1");
-    q.style.filter="none";
-
-    var r=document.getElementById("bigbox1");
-    r.remove();
-    illusion();
-
-}
+    let list = document.createElement("div")
+    list.setAttribute("class", "boxx")
+    list.setAttribute("id", val)
 
 
+    let tripp = document.createElement("p")
+    tripp.setAttribute("class", "trip")
+    tripp.innerHTML = val
 
+    let line = document.createElement("hr")
 
+    let databo = document.createElement("div")
+    databo.setAttribute("class", "databox")
+    databo.setAttribute("id", val + "in")
 
-const illusion=()=>{
-   console.log(cap);
-    var build=document.createElement("div");
-    build.setAttribute("class","child");
-    build.setAttribute("id",cap);
-   
-    
-    var build1=document.createElement("p");
-    build1.setAttribute("class","placee")
-    build1.setAttribute("id",cap+"place");
-    build1.innerHTML=cap;
+    let innerdatabo = document.createElement("div")
+    innerdatabo.setAttribute("class", "databoxin")
+    innerdatabo.setAttribute("id", val + "inner")
 
-    var line=document.createElement("hr");
+    let icon = document.createElement("span")
+    icon.setAttribute("class", val + "icco")
+    icon.setAttribute("onclick", "obtaindata(this.getAttribute('class'))")
+    icon.innerHTML = '<i class="fas fa-plus-square fa-2x aria-hidden="true" "></i>'
 
-    var build2=document.createElement("div");
-    build2.setAttribute("class","thingsudid");
-    build2.setAttribute("id",cap+"thingsudid")
-
-    var build3=document.createElement("div");
-    build3.setAttribute("class","write");
-    build3.setAttribute("id",cap+"write")
-
-    var icon1=document.createElement("span");
-    icon1.setAttribute("class","ico1")
-    icon1.setAttribute("id",cap+"ico1")
-    icon1.setAttribute("onclick","add(this.getAttribute('id'))");
-    icon1.innerHTML = '<i class="fas fa-plus-square fa-2x aria-hidden="true" "></i>'
-    
-    var icon2=document.createElement("span");
-    icon2.setAttribute("class","ico2")
-    icon2.setAttribute("id",cap+"ico2")
+    let icon2 = document.createElement("span")
+    icon2.setAttribute("class", val + "iccodel")
+    icon2.setAttribute("onclick", "delvalue(this.getAttribute('class'))")
     icon2.innerHTML = '<i class="fas fa-trash-alt fa-2x aria-hidden="true" "></i>'
 
-    build2.appendChild(build3);
-    build2.appendChild(icon1);
-    build2.appendChild(icon2);
-    build.appendChild(build1);
-    build.appendChild(line);
-    build.appendChild(build2);
-    document.getElementById("start").appendChild(build);
+    databo.appendChild(innerdatabo)
+    databo.appendChild(icon)
+    databo.appendChild(icon2)
+    list.appendChild(tripp)
+    list.appendChild(line)
+    list.appendChild(databo)
+    document.getElementById("innermain").appendChild(list)
 
 }
-function goo1(){
-    var q=document.getElementById("top1");
-    q.style.filter="none";
-    var r=document.getElementById("bigbox1");
-    r.remove();
-}
-var vall
-function add(el){
-    var bigboxx=document.createElement("div");
-    bigboxx.setAttribute("id","bigbox2");
-    
-    
-    var insideone=document.createElement("label");
-    insideone.innerHTML="ADD NEW LIST";
-    insideone.setAttribute("for","insideee");
-    bigboxx.appendChild(insideone);
+function obtainvalue() {
+    var inputbx = document.createElement("div")
+    inputbx.setAttribute("id", "inputbox")
 
-    var insidetwo=document.createElement("input");
-    insidetwo.setAttribute("placeholder","Enter the list")
-    insidetwo.setAttribute("id","insideee1");
-    insidetwo.setAttribute("name","text");
-    insidetwo.setAttribute("type","text");
-    bigboxx.appendChild(insidetwo);
-
-    var insidethree=document.createElement("button");
-    insidethree.innerHTML="ADD";
-    insidethree.setAttribute("id","insideee2");
-    insidethree.setAttribute("type","submit");
-    insidethree.setAttribute("onclick","secondclick()");
-    bigboxx.appendChild(insidethree);
-
-    var insidefour=document.createElement("button");
-    insidefour.innerHTML="CLOSE";
-    insidefour.setAttribute("id","insideee3");
-    insidefour.setAttribute("onclick","secondclick2()");
-    insidefour.setAttribute("type","submit");
-    bigboxx.appendChild(insidefour);
-
-    document.getElementById("top").appendChild(bigboxx);
-    vall=el
-    
-}
-
-var take;
-function secondclick(){
-    
-
-    var gasp=document.getElementById("insideee1");
-    take=gasp.value;
-    console.log(take);
-
-    var y=document.getElementById("top1");
-    y.style.filter="none";
-
-    var t=document.getElementById("bigbox2");
-    t.remove(length);
+    var labe = document.createElement("label")
+    labe.setAttribute("for", "datatrip")
+    labe.innerHTML = "Enter task list";
 
 
-    hurray()
-}
+    var inputfiel = document.createElement("input")
+    inputfiel.setAttribute("name", "datatrip")
+    inputfiel.setAttribute("id", "datatrip")
+    inputfiel.setAttribute("type", "text")
 
+    var butto = document.createElement("button")
+    butto.setAttribute("id", "adde")
+    butto.setAttribute("type", "submit")
+    butto.innerHTML = "add"
+    butto.setAttribute("onclick", "dop()")
 
-function hurray(){
+    var butto2 = document.createElement("button")
+    butto2.setAttribute("id", "del")
+    butto2.setAttribute("type", "submit")
+    butto2.innerHTML = "close"
+    butto2.setAttribute("onclick", "drop()")
 
 
 
+    inputbx.appendChild(labe)
+    inputbx.appendChild(inputfiel)
+    inputbx.appendChild(butto)
+    inputbx.appendChild(butto2)
 
-    var create1=document.createElement("p");
-    create1.setAttribute("class","innerp")
-    create1.setAttribute("id",cap+take+"master");
-    
-    var creater=document.createElement("span")
-    creater.setAttribute("id",take+"mark")
-    create1.innerHTML=take;
+    document.getElementById("main1").appendChild(inputbx)
+    var master = document.getElementById("main")
 
-    var create2=document.createElement("span");
-    create2.setAttribute("class","markdone")
-    create2.setAttribute("id",take+"markdone");
-    create2.setAttribute("onclick","change");
-    create2.innerHTML="markdone";
-
-    create1.appendChild(creater)
-    create1.appendChild(create2)
-     
-    
-     var find=document.getElementById(vall)
-     var got=find.previousElementSibling
-     got=got.id
-     document.getElementById(got).appendChild(create1)
+    master.style.filter = "blur(7px)"
 
 
 
 }
+let val;
+function dop() {
+    var a = document.getElementById("datatrip")
+    val = a.value
 
+    var master = document.getElementById("main")
+    master.style.filter = "none"
+    var del = document.getElementById("inputbox")
+    del.remove()
 
+    create()
 
 
+}
+function drop() {
 
+    var master = document.getElementById("main")
+    master.style.filter = "none"
+    var del = document.getElementById("inputbox")
 
+    del.remove()
 
+}
+let task;
+function againdop() {
+    var a = document.getElementById("datatrip")
+    task = a.value
 
+    var master = document.getElementById("main")
+    master.style.filter = "none"
+    var del = document.getElementById("inputbox")
 
+    del.remove()
 
+    hit()
 
 
 
 
 
+}
+function hit() {
 
+    var taskbo = document.createElement("p")
+    taskbo.setAttribute("class", "taskdata")
+    taskbo.setAttribute("id", val + task + "task")
 
+    var dat = document.createElement("span")
+    dat.setAttribute("class", "markdata")
+    dat.setAttribute('id', val + task + "markdata")
+    dat.innerText = task
 
 
+    var mar = document.createElement("span")
+    mar.setAttribute("class", "mark")
+    mar.setAttribute("id", val + task + "mark")
+    mar.setAttribute("onclick", "und(this.getAttribute('id'))")
+    mar.innerText = "mark"
 
+    var pe = document.getElementsByClassName(addres)
+    var i = pe[0].previousElementSibling
+    var re = i.id
 
 
 
+    taskbo.appendChild(dat)
+    taskbo.appendChild(mar)
+    document.getElementById(re).appendChild(taskbo)
+}
 
+function und(el) {
+    var i = document.getElementById(el)
+    var t = i.previousElementSibling
+    t.style.textDecorationLine = "line-through";
+    i.remove()
 
 
+}
+var addres
+function obtaindata(el) {
+    var inputbx = document.createElement("div")
+    inputbx.setAttribute("id", "inputbox")
 
+    var labe = document.createElement("label")
+    labe.setAttribute("for", "datatrip")
+    labe.innerHTML = "Enter task list";
 
 
+    var inputfiel = document.createElement("input")
+    inputfiel.setAttribute("name", "datatrip")
+    inputfiel.setAttribute("id", "datatrip")
+    inputfiel.setAttribute("type", "text")
 
+    var butto = document.createElement("button")
+    butto.setAttribute("id", "adde")
+    butto.setAttribute("type", "submit")
+    butto.innerHTML = "add"
+    butto.setAttribute("onclick", "againdop()")
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var zeroo=document.getElementById("newyork");
-// zeroo.style.visibility="hidden";
-
-// var zeroo1=document.getElementById("paris");
-// zeroo1.style.visibility="hidden";
-
-// var mylist=[];
-// function add(){
-//     var p1=document.getElementById("text").value;
-//     var tempObj={
-//         name:p1
-//     };
-//     mylist.push(tempObj);
-//     console.log(tempObj);
-//     addie();
-    
-// }
-// function addie(){
-//     var y=document.getElementById("box");
-//     y.style.visibility="hidden";
-    
-//     var zeroo=document.getElementById("newyork");
-//     zeroo.style.visibility="visible";
-
-//     const child=document.createElement("div");
-//     child.setAttribute("id","any");
-//     child.innerText=mylist[mylist.length-1].name;
-//     var zeroo=document.getElementById("newyork");
-//     zeroo.appendChild(child);
-//     // zeroo.remove();
-
-//     zeroo1=document.getElementById("paris");
-//     zeroo1.style.visibility="visible";
-
-//     const child1=document.createElement("div");
-//     child1.setAttribute("id","any1");
-//     child1.innerText=mylist[mylist.length-1].name;
-//     zeroo1.appendChild(child1);
-
-// }
-// function clic(){
-//     var y=document.getElementById("box");
-//     y.style.visibility="visible";
-    
-// }
-
-// var clicking=()=>{
-//     var b=document.getElementById("box");
-//     b.setAttribute("class","boxi");
-//     console.log(b);     
-//     var a=document.createElement("p");
-//     a.innerText="Add New List";
-//     a.setAttribute("class","box1");
-//     a.appendChild(b);
-
-    
-//     var d=document.createElement("input");
-//     d.setAttribute("type","text");
-//     d.setAttribute("class","box2");
-//     d.setAttribute("id","text2");
-//     d.placeholder="add new list";
-    
-//     console.log(d);
-   
-//     var c=document.getElementById("box").appendChild(d);
-//     console.log(c);
-
-//     var e=document.createElement("button");
-//     e.setAttribute("class","box3");
-//     e.innerHTML="Add";
-//     // e.onclick="add()";
-//     // e.getElementById("text2").value;
-//     // console.log(o);
-//     var f=document.getElementById("box").appendChild(e);
-
-//     console.log(e);
-
-//     var g=document.createElement("button");
-//     g.setAttribute("class","box4");
-//     g.innerText="Close";
-//     console.log(g);
-//     var h=document.getElementById("box").appendChild(g);
-//     console.log(g);  
-
-    
-    
-//     var i=document.getElementById("box");
-//     console.log(i);
-
-    
-// }
-
-
-
-
+    var butto2 = document.createElement("button")
+    butto2.setAttribute("id", "del")
+    butto2.setAttribute("type", "submit")
+    butto2.innerHTML = "close"
+    butto2.setAttribute("onclick", "drop()")
+
+
+    addres = el
+
+
+
+    inputbx.appendChild(labe)
+    inputbx.appendChild(inputfiel)
+    inputbx.appendChild(butto)
+    inputbx.appendChild(butto2)
+    document.getElementById("main1").appendChild(inputbx)
+    var master = document.getElementById("main")
+
+    master.style.filter = "blur(7px)"
+
+
+}
+
+function delvalue(el) {
+    var i = document.getElementsByClassName(el)
+    var a = i[0].previousElementSibling
+    var b = a.previousElementSibling
+    b = b.id
+
+    let re = document.getElementById(b)
+    var master = re.childElementCount
+    if (master != 0) {
+        var select = document.getElementById(b)
+        select.removeChild(select.lastChild)
+    }
+    else {
+        var main = document.getElementById(b)
+        var mmain = main.parentNode.parentNode
+        var parentbox = mmain.id
+        mainbox = document.getElementById(parentbox)
+        var isemp = mainbox.parentNode
+        mainbox.remove()
+        var count = isemp.childElementCount
+        if (count == 0) {
+            var empty = document.createElement("span")
+            empty.setAttribute("id", "empty")
+            empty.innerText = "task list is empty"
+            isemp.appendChild(empty)
+        }
+    }
+
+}
